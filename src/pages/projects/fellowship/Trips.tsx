9dc576db2eafc9f1dc7dac9d5f7a694c15e5c5b2
Bezuayehu Mamuye <bezuayehum@act.com.et>
2025-04-16 16:59:08 +0300

    small fix

--- src/pages/projects/fellowship/Trips.tsx
+++ src/pages/projects/fellowship/Trips.tsx
@@ -5,12 +5,12 @@ import { Project } from '../../../types';
 
 import trip from "../../../assets/Trip/photo_2025-04-06_14-16-59.jpg";
 import trip1 from "../../../assets/Trip/pic1.jpg";
-import trip2 from '../../../assets/Trip/abc_3247.png'
+// import trip2 from '../../../assets/Trip/abc_3247.png'
 import trip3 from "../../../assets/Trip/photo_2025-04-05_20-12-53.jpg";
 import trip4 from "../../../assets/Trip/photo_2025-04-15_21-47-33.jpg";
-import trip5 from "../../../assets/Trip/abc_3249.png";
-import trip6 from "../../../assets/Trip/abc_3250.png";
-import trip7 from "../../../assets/Trip/abc_3258.png";
+// import trip5 from "../../../assets/Trip/abc_3249.png";
+// import trip6 from "../../../assets/Trip/abc_3250.png";
+// import trip7 from "../../../assets/Trip/abc_3258.png";
 
 const Trips = () => {
   const projectData: Project = {
@@ -25,7 +25,7 @@ const Trips = () => {
       "100% member participation rate",
       "Memorable team experiences"
     ],
-    gallery:[`${trip1}`,`${trip}`,`${trip2}`,`${trip3}`,`${trip4}`,`${trip5}`,`${trip6}`,`${trip7}`],
+    gallery:[`${trip1}`,`${trip}`,`${trip3}`,`${trip4}`],
     outcomes: [
       { label: "Destinations Visited", value: "15+" },
       { label: "New Leaders Trained", value: "20+/year" }
